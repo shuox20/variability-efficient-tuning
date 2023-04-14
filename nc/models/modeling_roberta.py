7589f12ea24c366b9aaf196b226576ca114d4f00
Shuo <shuox@uchicago.edu>
2023-04-14 00:41:44 -0500

    adapter

--- nc/models/modeling_roberta.py
+++ nc/models/modeling_roberta.py
@@ -24,8 +24,8 @@ from packaging import version
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 
-from transformers.models.activations import ACT2FN, gelu
-from transformers.models.modeling_outputs import (
+from transformers.activations import ACT2FN, gelu
+from transformers.modeling_outputs import (
     BaseModelOutputWithPastAndCrossAttentions,
     BaseModelOutputWithPoolingAndCrossAttentions,
     CausalLMOutputWithCrossAttentions,
@@ -37,7 +37,7 @@ from transformers.models.modeling_outputs import (
 )
 from transformers.modeling_utils import PreTrainedModel
 from transformers.pytorch_utils import apply_chunking_to_forward, find_pruneable_heads_and_indices, prune_linear_layer
-from transformers.models.utils import (
+from transformers.utils import (
     add_code_sample_docstrings,
     add_start_docstrings,
     add_start_docstrings_to_model_forward,

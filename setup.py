cb58159e1ed52237913727de4a5b02eb8ce50521
Shuo <shuox@uchicago.edu>
2023-04-14 01:33:00 -0500

    setup

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
     version='1.0',
     packages=setuptools.find_packages(),
     install_requires=[
+        'torch==1.11.0',
+        'sklearn',
         'transformers==4.19.0',
         'datasets',
-        'accelerate',
-        'sklearn'
+        'accelerate'
     ]
     )
